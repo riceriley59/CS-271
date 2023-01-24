6332ceac2af106fe5940d2fd9a431a668a0718c9
Riley Rice <riceriley59@gmail.com>
2023-01-24 12:24:31 -0800

    added functionality to assignment 2

--- Assignment2.asm
+++ Assignment2.asm
@@ -2,9 +2,7 @@ TITLE Program Assignment2(Assignment2.asm)
 
 ; Author(s) : Riley Rice	Date : 1 - 17 - 2023
 ; Course: CS271
-; Description: This displays my name and program title and then gets two numbers from
-; users and then caclulates the sum, difference, product, integer quotient and remainder
-; of the numbers then say goodbye. The first number must be bigger than the second number.
+; Description: 
 
 INCLUDE Irvine32.inc
 
@@ -13,24 +11,31 @@ INCLUDE Irvine32.inc
 .stack 4096			;SS register
 ExitProcess proto, dwExitCode:dword
 
-.data
+.data               ;DS register
     ;introduction
     intro1 BYTE "Fibonacci Numbers",0
     intro2 BYTE "Riley Rice",0
 
     ;displaying instructions and getting user's name
-    askForName BYTE "What's your name?",0
+    askForName BYTE "What's your name?: ",0
     hello BYTE "Hello, ",0
     instructions BYTE "Enter the number of Fibonacci terms to be displayed give the number as an integer in the range [1 .. 46].",0
+    uName BYTE 15 DUP(0)
+
+    ;displaying fibs
+    temp DWORD ?
+    spacing BYTE "   ",0
 
     ;get user data
-    askForNumber BYTE "How many Fibonacci terms do you want?",0
+    askForNumber BYTE "How many Fibonacci terms do you want? ",0
+    out_of_range BYTE "That number is out of range enter a number in range [1 .. 46]",0
+    fibNumbers DWORD ?
 
     ;farewell
     certification BYTE "Results certified by Riley Rice.",0
     goodbye BYTE "Goodbye, ",0
 
-.code
+.code               ;CS register
 main proc
     introduction:				;This is the introduction section
 		;display intro one prompt
@@ -44,13 +49,87 @@ main proc
 		call Crlf
         call Crlf
 
-    userInstructions:
+    userInstructions:           ;This gets the user's name and then displays the instructions
+        mov EDX, OFFSET askForName
+        call WriteString
+        
+        mov EDX, OFFSET uName
+        mov ECX, 15
+        call ReadString
+        
+        mov EDX, OFFSET hello
+        call WriteString
+        mov EDX, OFFSET uName
+        call WriteString
+        call Crlf
 
-    getUserData:
+        mov EDX, OFFSET instructions
+        call WriteString
+        call Crlf
+        call Crlf
 
-    displayFibs:
+    getUserData:                ;This gets the amount of fib numbers that the user wants and validates that input
+        mov EDX, OFFSET askForNumber
+        call WriteString
 
-    farewell:
+        call ReadInt
+        mov fibNumbers, EAX
+
+        mov EAX, fibNumbers
+        cmp EAX, 47
+        JG error
+        jmp out_of_error
+        
+        error:
+            call Clrf
+            mov EDX, OFFSET out_of_range
+            call WriteString
+            call Clrf
+
+            jmp getUserData
+        out_of_error:
+            cmp EAX, 1
+            JL error
+
+    displayFibs:                ;This will handle displaying the fib numbers
+        mov ECX, fibNumbers
+
+        mov EAX, 1
+        mov EBX, 1
+        mov EDI, 1
+
+        mov EDX, OFFSET spacing
+
+        fibloop:
+            call WriteDec
+            call WriteString
+
+            inc EDI
+
+            cmp EDI, 4
+            je newline
+            jmp continue
+            newline:
+                call Crlf
+                mov EDI, 1
+            continue:
+
+            mov temp, EBX
+            add EBX, EAX
+            mov EAX, temp
+
+            loop fibloop
+
+    farewell:                   ;This says goodbye to the user before ending the program
+        mov EDX, OFFSET certification
+        call WriteString
+        call Crlf
+        
+        mov EDX, OFFSET goodbye
+        call WriteString
+
+        mov EDX, OFFSET uName
+        call WriteString
 
     ;end program and return 0 to operating system
 	invoke ExitProcess, 0
