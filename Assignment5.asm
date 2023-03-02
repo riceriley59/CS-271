9f51bf6359215e326b5ae13b76558950683d1dc4
Riley Rice <riceriley59@gmail.com>
2023-03-02 10:53:52 -0800

    bug

--- Assignment5.asm
+++ Assignment5.asm
@@ -39,7 +39,7 @@ HI = 999
      goodbyeS BYTE "Results certified by Riley Rice. Goodbye.",0
 
      ;array and requested size
-     request DWORD 0
+     request DWORD ?     
      array DWORD MAX DUP(?)
 
 
